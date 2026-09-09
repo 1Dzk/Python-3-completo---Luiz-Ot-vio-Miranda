ccb98f0ee37a79423ce7402e4f385028934e0c69
Henrique <henripereira922@gmail.com>
2026-09-09 11:21:31 -0300

    Introdução à List comprehension em Python  - Aula45

---
+++ Códigos/Modulo 2/aula45.py
@@ -0,0 +1,16 @@
+# List comprehension em Python
+# List comprehension é uma forma concisa de criar listas
+# a partir de iteráveis.
+
+lista = []
+
+for numero in range(10):
+    lista.append(numero)
+#print(lista)
+
+
+lista = [
+    numero * 2
+    for numero in range(10)
+]
+print(lista)
