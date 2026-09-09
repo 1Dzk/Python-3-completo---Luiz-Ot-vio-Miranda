0885588893078dc4499147a63c4532f5d65e3696
Henrique <henripereira922@gmail.com>
2026-09-09 10:41:00 -0300

    Funções lambda complexas - Aula43

---
+++ Códigos/Modulo 2/aula43.py
@@ -0,0 +1,34 @@
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+def soma(a, b):
+    return a + b
+
+#def cria_multiplicador(multiplicador):
+ #   def multiplicar(numero):
+ #       return numero * multiplicador
+   # return multiplicar
+
+#duplica = cria_multiplicador(2)
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+)
+print(duplica(2))
+
+
+print(
+    executa(
+        lambda a,b: a + b,
+        2,3
+    )
+)
+
+
+print(
+    executa(
+        lambda * args: sum(args),
+        1,2,3,4,5,6,7
+    )
+)
