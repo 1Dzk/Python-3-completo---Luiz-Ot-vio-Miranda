cc6df08a8b0aa1b50f9b593ff3aa5b500cc8fecd
Henrique <henripereira922@gmail.com>
2026-09-22 12:03:44 -0300

    Dictionary Comprehension e Set Comprehension - Aula49

---
+++ Códigos/Modulo 2/aula49.py
@@ -0,0 +1,32 @@
+# Dicionary Comprehension e Set Comprehension
+produto = {
+    'nome': 'Caneta Azul',
+    'preco': 2.50,
+    'categoria': 'Escritório',
+}
+
+dc = { 
+      chave: valor.upper()
+      if isinstance(valor, str) else valor
+      for chave , valor 
+      in produto.items()
+      if chave != 'categoria'
+      
+}
+
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor b'),
+    ('c', 'valor c'),
+    
+]
+dc = {
+  chave : valor 
+  for chave, valor in
+  lista
+  
+ }
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
